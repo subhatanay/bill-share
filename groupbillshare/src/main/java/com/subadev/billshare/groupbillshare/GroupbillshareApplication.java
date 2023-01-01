01ae67a6d35bdaeadf4995915c428410953017d6
Subhajit Ghosh <subhajgh@cisco.com>
2023-01-01 21:17:37 +0530

    Inital commit - userservice, groupbillshare, notificationservice

---
+++ groupbillshare/src/main/java/com/subadev/billshare/groupbillshare/GroupbillshareApplication.java
@@ -0,0 +1,13 @@
+package com.subadev.billshare.groupbillshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupbillshareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupbillshareApplication.class, args);
+    }
+
+}
---
+++ groupbillshare/src/test/java/com/subadev/billshare/groupbillshare/GroupbillshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subadev.billshare.groupbillshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupbillshareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
